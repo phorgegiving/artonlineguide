c8447129d879ef4257ca2ee9344e5e0c337d7266
vandeurenglenn <vandeurenglenn@gmail.com>
2022-05-19 18:49:43 +0200

    Add shared-styles

---
+++ src/shared-styles/shared.js
@@ -0,0 +1,12 @@
+export const scrollbar = `
+  ::-webkit-scrollbar {
+    width: 12px;
+  }
+  ::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    -webkit-box-shadow: inset 0 0 6px rgb(215 16 150 / 50%);
+  }
+  ::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgb(225 225 225 / 30%);
+    border-radius: 10px;
+  }`
